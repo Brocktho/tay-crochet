aa71d1fcaf22454912535ab7bc16e0523b90e95e
Brocktho <bcd5235@psu.edu>
2025-04-01 20:49:07 -0400

    deploy init

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -22,6 +22,19 @@ export default function Index({ loaderData }: Route.ComponentProps) {
 				src="/img/TayCrochetBanner.jpg"
 				alt="Welcome To Tay Crochet"
 			/>
+			<h1 className="text-xl">Welcome!</h1>
+			<p className="w-full text-center text-lg">
+				I’m Taylor, and I create adorable, handmade crochet plushies, perfect
+				for gifting or adding a little charm to your space. Right now, I
+				specialize in basic animal designs, but I’m more than happy to modify
+				them to fit your vision! Each plushie is crafted with love and attention
+				to detail, though I’m not a professional – I’m just doing what I love!
+				While they might not be perfect, I promise to always do my best to
+				create something special just for you. If you have a custom plushie idea
+				in mind, feel free to reach out – I’d love to try my best to bring your
+				vision to life, as long as I believe I can provide you with a quality
+				product. Take a look around, and let’s create something unique together!
+			</p>
 			<div className={'flex w-full flex-row flex-wrap justify-center gap-3'}>
 				{products.map((product) => {
 					return <div key={product.id} className={'flex flex-1'}></div>
