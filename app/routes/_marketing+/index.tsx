4ab0862f22aeab103e3a1d8a9cb8fab9e43e527a
Brocktho <brock.donahue123+github.notifications@gmail.com>
2025-04-03 07:36:42 -0400

    some landing page changes

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -2,6 +2,7 @@ import { type Route } from './+types/index.ts'
 import { logos } from './logos/logos.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { Outlet } from 'react-router'
+import { getProductImgSrc } from '#app/utils/misc.tsx'
 
 export const meta: Route.MetaFunction = () => [{ title: 'Tay Crochet' }]
 
@@ -21,7 +22,17 @@ export default function Index({ loaderData }: Route.ComponentProps) {
 		<main className="font-poppins grid h-full place-items-center">
 			<div className={'flex w-full flex-row flex-wrap justify-center gap-3'}>
 				{products.map((product) => {
-					return <div key={product.id} className={'flex flex-1'}></div>
+					const primaryVariant = product.variants[0]!
+					return (
+						<div key={product.id} className={'flex flex-1 flex-col'}>
+							<img
+								className={'h-48 rounded-t-lg object-cover'}
+								alt={primaryVariant.imageAltText}
+								src={getProductImgSrc(primaryVariant.id)}
+							/>
+							<p>{product.title}</p>
+						</div>
+					)
 				})}
 			</div>
 			<div className={'h-32 w-32 rounded-b-xl bg-primary'}>a</div>
